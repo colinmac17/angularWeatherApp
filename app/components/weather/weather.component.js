dd9e8e195c71e4d28f54b5fee3d6271844af7421
Colin McAtee <colin.g.mcatee@gmail.com>
2018-07-22 22:45:30 -0500

    have current weather working well. moving onto forecast

--- app/components/weather/weather.component.js
+++ app/components/weather/weather.component.js
@@ -5,13 +5,30 @@ angular.
     templateUrl: 'components/weather/weather.template.html',
     controller: ['$http', function WeatherController($http) {
        var self = this;
+       self.isLoading = true;
        init(self, function(){
+           self.isLoading = false;
             self.latitude = JSON.parse(sessionStorage.getItem('latitude'));
             self.longitude = JSON.parse(sessionStorage.getItem('longitude'));
-            $http.get(`https://api.openweathermap.org/data/2.5/weather?lat=${self.latitude}&lon=${self.longitude}&units=imperial&appid=9e147d5030e6fcdae1ae8c95e25ee211`).then(function(response){
+
+            //Get Current Weather
+            $http.get(`http://api.openweathermap.org/data/2.5/weather?lat=${self.latitude}&lon=${self.longitude}&units=imperial&appid=9e147d5030e6fcdae1ae8c95e25ee211`).then(function(response){
+                //console.log(response);
+                self.temp = response.data.main.temp;
+                self.locationName = response.data.name;
+                let now = new Date();
+                let today = now.getDate();
+                let month = now.getMonth() + 1;
+                let year = now.getFullYear();
+                self.dateTime = `${month}/${today}/${year}`;
+                self.desc = response.data.weather[0].description;
+                self.icon = `http://openweathermap.org/img/w/${response.data.weather[0].icon}.png`
+            });
+
+            //Get Forecast
+            $http.get(`http://api.openweathermap.org/data/2.5/forecast?lat=${self.latitude}&lon=${self.longitude}&units=imperial&appid=9e147d5030e6fcdae1ae8c95e25ee211`).then(function(response){
                 console.log(response);
-                var { temp } = response.data.main;
-                self.temp = temp;
+                
             });
        });
       }
